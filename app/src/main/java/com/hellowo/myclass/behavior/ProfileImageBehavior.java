47f2c23f185dd632e2891b2aa34f89a78383d7de
hellowo <dayfly86@gmail.com>
2017-03-23 08:22:47 +0900

    커밋

--- app/src/main/java/com/hellowo/myclass/behavior/ProfileImageBehavior.java
+++ app/src/main/java/com/hellowo/myclass/behavior/ProfileImageBehavior.java
@@ -2,6 +2,7 @@ package com.hellowo.myclass.behavior;
 
 import android.animation.Animator;
 import android.content.Context;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.view.ViewCompat;
@@ -12,22 +13,35 @@ import android.view.View;
 import android.view.ViewPropertyAnimator;
 import android.view.animation.Interpolator;
 
+import com.hellowo.myclass.AppScreen;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 
-public class ProfileImageBehavior extends CoordinatorLayout.Behavior<View> {
+public class ProfileImageBehavior extends CoordinatorLayout.Behavior<CircleImageView> {
+    int startX;
+    int startY;
+    int minHeaderHeight;
 
     public ProfileImageBehavior(Context context, AttributeSet attrs) {
         super(context, attrs);
+        startX = AppScreen.getDeviceWidth(context) / 2 - AppScreen.dpToPx(200) / 2;
+        startY = AppScreen.dpToPx(50);
+        minHeaderHeight = AppScreen.dpToPx(70);
     }
 
     @Override
-    public boolean onDependentViewChanged(CoordinatorLayout parent, View child, View dependency) {
-        return dependency instanceof CollapsingToolbarLayout;
+    public boolean onDependentViewChanged(CoordinatorLayout parent, CircleImageView child, View dependency) {
+        return dependency instanceof AppBarLayout;
     }
 
     @Override
-    public boolean layoutDependsOn(CoordinatorLayout parent, View child, View dependency) {
+    public boolean layoutDependsOn(CoordinatorLayout parent, CircleImageView child, View dependency) {
+        if(dependency instanceof AppBarLayout){
+            Log.i("minHeaderHeight", "CollapsingToolbarLayout");
+        }
+            Log.i("minHeaderHeight", ""+minHeaderHeight);
         Log.i("aaa", ""+dependency.getY());
+        child.setX(startX);
         return false;
     }
 }
